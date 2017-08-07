1061ab02bdc3f181a84e80e2d24b15a204df7d60
gct <gct@yonyou.com>
2017-08-07 14:34:39 +0800

    tt

---
+++ scp_kb.sh
@@ -0,0 +1,5 @@
+scp -r /Users/gct/work/gho/kb_10_3_13_4/kb/* root@10.3.13.4:/home/gct/apache-tomcat-8.5.16/webapps/kb/
+
+scp -r /Users/gct/work/gho/kb_10_3_13_4/kb_home/yycloudkbcore/*  root@10.3.13.4:/home/gct/kb_home/yycloudkbcore/
+
+scp -r /Users/gct/work/gho/kb_10_3_13_4/kb/WEB-INF/lib/*.jar root@10.3.13.4:/home/gct/apache-tomcat-8.5.16/webapps/kb/WEB-INF/lib/
